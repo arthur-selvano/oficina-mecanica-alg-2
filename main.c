f8388cafef1462623720f2cdf57c7b550878d667
Arthur Selvano <arthurselvano@outlook.com.br>
2026-06-16 20:24:31 -0300

    feat: adiciona 3 estrutura

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <string.h> 
 
-int main(){
+// 1. Estrutura Mecânico
+struct mecanico {
+    int id_mecanico;       
+    char nome[100];
+    char especialidade[100];    
+    float salario;         
+};
 
-    printf("Teste GitHub!\n");
+// 2. Estrutura Veículo
+struct veiculo {
+    char placa_veiculo[15]; 
+    char modelo[50];
+    int ano;
+    char telefone_dono[20];
+};
+
+// 3. Estrutura Ordem de Serviço
+struct ordem_de_servico {
+    int numero_os;          
+    int id_mecanico;        
+    char placa_veiculo[15]; 
+    char data_servico[11];  
+    float valor_total;      
+};
+
+
+int main(){    
 
     return 0;
 }
